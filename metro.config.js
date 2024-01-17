e917780a3f958e7a9071097c3074b31203b8106b
ashwamegh <shashank7200@gmail.com>
2024-01-17 22:29:11 +0530

    init commit

---
+++ metro.config.js
@@ -0,0 +1,52 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const {getDefaultConfig} = require('metro-config');
+
+const jsoMetroPlugin = require('obfuscator-io-metro-plugin')(
+  {
+    // for these option look javascript-obfuscator library options from  above url
+    compact: false,
+    sourceMap: false,
+    controlFlowFlattening: true,
+    controlFlowFlatteningThreshold: 1,
+    numbersToExpressions: true,
+    simplify: true,
+    stringArrayShuffle: true,
+    splitStrings: true,
+    stringArrayThreshold: 1,
+  },
+  {
+    runInDev: false /* optional */,
+    logObfuscatedFiles: true /* optional generated files will be located at ./.jso */,
+    // source Map generated after obfuscation is not useful right now
+    sourceMapLocation:
+      './index.android.bundle.map' /* optional  only works if sourceMap: true in obfuscation option */,
+  },
+);
+
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
