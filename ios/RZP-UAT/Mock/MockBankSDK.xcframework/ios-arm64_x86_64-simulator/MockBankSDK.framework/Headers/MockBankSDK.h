9c9c30c8f53a02d4278b0d6f878ed78f368d6394
ashwamegh <shashank7200@gmail.com>
2024-04-30 19:07:51 +0530

    added razorpay frameworks

---
+++ ios/RZP-UAT/Mock/MockBankSDK.xcframework/ios-arm64_x86_64-simulator/MockBankSDK.framework/Headers/MockBankSDK.h
@@ -0,0 +1,18 @@
+//
+//  MockBankSDK.h
+//  MockBankSDK
+//
+//  Created by Justin Joseph on 08/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MockBankSDK.
+FOUNDATION_EXPORT double MockBankSDKVersionNumber;
+
+//! Project version string for MockBankSDK.
+FOUNDATION_EXPORT const unsigned char MockBankSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockBankSDK/PublicHeader.h>
+
+
